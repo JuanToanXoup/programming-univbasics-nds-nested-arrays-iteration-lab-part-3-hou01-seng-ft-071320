43a94be70e22107b22ca4d44544511c8b095aa18
John Doan <johndoan42@gmail.com>
2020-06-22 14:38:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,9 +7,8 @@ def join_nested_strings(src)
     element_index = 0
     while element_index < src[row_index].count do
       if src[row_index][element_index].class == String
-        word = src[row_index][element_index] + ' '
+        sentence += word
       end
-      sentence += word
       element_index += 1
     end
     row_index +=1
